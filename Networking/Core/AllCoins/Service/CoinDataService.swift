f063e921cbcb64854845a9432136950966419135
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-07 19:01:11 +0100

    Complete the generic method to parse data from API

--- Networking/Core/AllCoins/Service/CoinDataService.swift
+++ Networking/Core/AllCoins/Service/CoinDataService.swift
@@ -11,32 +11,17 @@ class CoinDataService {
     private let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=24h&locale=en"
 
     func fetchCoins() async throws -> [Coin] {
-        let data = try await fetchData(endpoint: urlString)
+        return try await fetchData(as: [Coin].self, endpoint: urlString)
 
-        do {
-            let coins = try JSONDecoder().decode([Coin].self, from: data)
-            return coins
-        } catch {
-            print("DEBUG: Error fetching data: \(error)")
-            throw error as? CoinApiError ?? .unknownError(error: error)
-        }
     }
 
     func fetchCoinDetails(id: String) async throws -> CoinDetails? {
-
         let detailsURLString = "https://api.coingecko.com/api/v3/coins/\(id)?localization=false"
-        let data = try await fetchData(endpoint: detailsURLString)
+        return try await fetchData(as: CoinDetails.self, endpoint: detailsURLString)
 
-        do {
-            let details = try JSONDecoder().decode(CoinDetails.self, from: data)
-            return details
-        } catch {
-            print("DEBUG: Error: \(error)")
-            throw error as? CoinApiError ?? .unknownError(error: error)
-        }
     }
 
-    private func fetchData(endpoint: String) async throws -> Data {
+    private func fetchData<T: Codable>(as type: T.Type, endpoint: String) async throws -> T {
         guard let url = URL(string: endpoint) else {
             throw CoinApiError.requestFailed(description: "Invalid URL")
         }
@@ -51,6 +36,11 @@ class CoinDataService {
             throw CoinApiError.invalidStatusCode(statusCode: httpResponse.statusCode)
         }
 
-        return data
+        do {
+            return try JSONDecoder().decode(type, from: data)
+        } catch {
+            throw error as? CoinApiError ?? .unknownError(error: error)
+        }
+
     }
 }
