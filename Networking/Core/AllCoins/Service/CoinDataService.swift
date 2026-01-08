409cbb7f65a72a1164942c2ca8b6b97c5fa12047
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-08 10:30:29 +0100

    Implement cache in CoinDataService

--- Networking/Core/AllCoins/Service/CoinDataService.swift
+++ Networking/Core/AllCoins/Service/CoinDataService.swift
@@ -16,10 +16,19 @@ class CoinDataService: HTTPDataDownloader {
     }
 
     func fetchCoinDetails(id: String) async throws -> CoinDetails? {
+        if let cached = CoinDetailsCache.shared.get(forkey: id) {
+            print("DEBUG: Got details from CACHE.")
+            return cached
+        }
+
         guard let endpoint = coinDetailsURLString(id: id) else {
             throw CoinApiError.requestFailed(description: "Invalid Endpoint")
         }
-        return try await fetchData(as: CoinDetails.self, endpoint: endpoint)
+
+        let details = try await fetchData(as: CoinDetails.self, endpoint: endpoint)
+        print("DEBUG: Got details from API.")
+        CoinDetailsCache.shared.set(details, forkey: id)
+        return details
     }
 
     private var baseURLComponents: URLComponents {
