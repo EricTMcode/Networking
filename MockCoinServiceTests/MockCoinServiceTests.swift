47cd886e4030d3cf8ef76a495f00f7dd523e18dc
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-08 14:15:53 +0100

    first commit

---
+++ MockCoinServiceTests/MockCoinServiceTests.swift
@@ -0,0 +1,22 @@
+//
+//  MockCoinServiceTests.swift
+//  MockCoinServiceTests
+//
+//  Created by Eric on 08/01/2026.
+//
+
+import XCTest
+@testable import Networking
+
+final class MockCoinServiceTests: XCTestCase {
+
+    func test_DecodeCoinsIntoArray_marketCapDesc() throws {
+        let coins = try JSONDecoder().decode([Coin].self, from: mockCoinsData_marketCapDesc)
+        XCTAssertTrue(coins.count > 0)
+        XCTAssertFalse(coins.isEmpty)
+        XCTAssertEqual(coins.count, 20)
+
+        XCTAssertEqual(coins, coins.sorted(by: { $0.marketCapRank < $1.marketCapRank }))
+    }
+
+}
