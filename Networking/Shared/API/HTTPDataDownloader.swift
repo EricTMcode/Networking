7a4f93a52bc74db21fdec35672d10f22313f983b
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2026-01-08 10:14:19 +0100

    Refactor code

--- Networking/Shared/API/HTTPDataDownloader.swift
+++ Networking/Shared/API/HTTPDataDownloader.swift
@@ -16,22 +16,22 @@ extension HTTPDataDownloader {
         guard let url = URL(string: endpoint) else {
             throw CoinApiError.requestFailed(description: "Invalid URL")
         }
-
+        
         let (data, response) = try await URLSession.shared.data(from: url)
-
+        
         guard let httpResponse = response as? HTTPURLResponse else {
             throw CoinApiError.requestFailed(description: "Request failed")
         }
-
+        
         guard httpResponse.statusCode == 200 else {
             throw CoinApiError.invalidStatusCode(statusCode: httpResponse.statusCode)
         }
-
+        
         do {
             return try JSONDecoder().decode(type, from: data)
         } catch {
             throw error as? CoinApiError ?? .unknownError(error: error)
         }
-
+        
     }
 }
